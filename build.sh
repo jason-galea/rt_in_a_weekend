ce5f3334128d24f5a4392ab1527204da166ab518
JG <jasongalea98@outlook.com>
2023-04-16 13:33:43 +1000

    Lots of boilerplate, plus example static PPM image

--- build.sh
+++ build.sh
@@ -1,5 +1,47 @@
 #!/bin/bash
 
-g++ main.cpp -o out
+### Prep
+# sudo apt update -y
+# sudo apt install build-essential netpbm
 
-./out
+
+### Vars
+BINARY_FILE="./out"
+PPM_FILE="./images/out.ppm"
+PNG_FILE="./images/out.png"
+
+
+# ### Delete old binary
+# if [[ -f $BINARY_FILE ]]; then
+#     echo -e "\n==> $ rm $BINARY_FILE"
+#     rm $BINARY_FILE
+# fi
+
+
+# ### Delete old images
+# if [[ -f $PPM_FILE ]]; then
+#     echo -e "\n==> $ rm ./images/*.p*"
+#     rm ./images/*.p*
+# fi
+
+
+### Compile
+echo -e "\n==> $ g++ main.cpp -o $BINARY_FILE"
+g++ main.cpp -o $BINARY_FILE
+
+
+### Run new binary
+if [[ -f $BINARY_FILE ]]; then
+    echo -e "\n==> $ $BINARY_FILE"
+    $BINARY_FILE
+fi
+
+
+### Convert PPM file to PNG
+if [[ -f $PPM_FILE ]]; then
+    echo -e "\n==> $ pnmtopng $PPM_FILE > $PNG_FILE"
+    pnmtopng $PPM_FILE > $PNG_FILE
+fi
+
+
+echo
--- main.cpp
+++ main.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
 
 int main() {
 
+    // Constants
     const int image_height = 144;
     const int image_width = static_cast< int >( image_height * 16 / 9 );
+    const std::string output_path = "images/out.ppm";
 
-    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+    // Vars
+    std::stringstream output_ppm;
+    // output_ppm << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+    // EXAMPLE PPM
+    output_ppm << "P3\n"; // Format
+    output_ppm << "3 2\n"; // Resolution
+    output_ppm << "255\n"; // Max colour
+    output_ppm << "255  0   0       0   255 0       0   0   255\n"; // RGB values (row 1)
+    output_ppm << "255  255 0       255 255 255     0   0   0\n"; // RGB values (row 2)
+
+
+    // Save PPM    
+    std::cout << "INFO: Saving PPM data to " << output_path << "\n";
+    std::ofstream output_file_handler(output_path);
+    output_file_handler << output_ppm.str();
     
     
 
 
-
+    return 0;
 }
