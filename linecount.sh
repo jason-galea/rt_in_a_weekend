d9109e8cefb623bd1fbed92cb6a290b6a21911a1
JG <jasongalea98@outlook.com>
2023-04-16 17:20:09 +1000

    Update linecount script, trying to distract myself haha

--- linecount.sh
+++ linecount.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-echo -e "\n==> Linecount for all *.cpp and *.h files:"
-find . -type f \( -name "*.cpp" -o -name "*.h" \) | xargs -n1 cat | wc -l
+echo -e "\n==> Searching for '*.cpp' and '*.h' files"
+CPP_FILES=$(find $PWD -type f \( -name "*.cpp" -o -name "*.h" \) | sort)
 
+echo -e "\n==> Linecount for individual files:"
+echo $CPP_FILES | xargs -n1 wc -l
 
-echo -e "\n==> Linecount for all *.cpp and *.h files (Excluding blank lines & comments):"
-find . -type f \( -name "*.cpp" -o -name "*.h" \) | xargs -n1 cat | grep -vE "^$|^\ *\/\/" | wc -l
+echo -e "\n==> Linecount for individual files (Excluding blank lines & comments):"
+echo $CPP_FILES | xargs -n1 echo | xargs -I {} sh -c "grep -vE '^$|^\ *\/\/' {} | wc -l"
+
+echo -e "\n==> Total linecount:"
+echo $CPP_FILES | xargs -n1 cat | wc -l
+
+echo -e "\n==> Total linecount (Excluding blank lines & comments):"
+echo $CPP_FILES | xargs -n1 cat | grep -vE '^$|^\ *\/\/' | wc -l
 
 echo
