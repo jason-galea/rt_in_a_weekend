1185dc92fe22511785523d9172929050fdea1777
JG <jasongalea98@outlook.com>
2023-04-16 14:10:59 +1000

    Item 2.1 done

--- build.sh
+++ build.sh
@@ -4,27 +4,23 @@
 # sudo apt install build-essential netpbm
 
 
-### Vars
 BINARY_FILE="./out"
 PPM_FILE="./images/out.ppm"
 PNG_FILE="./images/out.png"
 
 
-### Compile
-echo -e "\n==> $ g++ main.cpp -o $BINARY_FILE"
+echo -e "\n==> Compiling binary"
 g++ main.cpp -o $BINARY_FILE
 
 
-### Run
 if [[ -f $BINARY_FILE ]]; then
-    echo -e "\n==> $ $BINARY_FILE"
+    echo -e "\n==> Executing binary"
     $BINARY_FILE
 fi
 
 
-### Convert PPM --> PNG
 if [[ -f $PPM_FILE ]]; then
-    echo -e "\n==> $ pnmtopng $PPM_FILE > $PNG_FILE"
+    echo -e "\n==> Converting PPM image to PNG"
     pnmtopng $PPM_FILE > $PNG_FILE
 fi
 
--- main.cpp
+++ main.cpp
@@ -2,32 +2,54 @@
 #include <sstream>
 #include <fstream>
 
+
+// Map float in range (0 --> 1) to int in range (0 --> 255)
+int map_float_to_rgb_int(float rgb_val) {
+    return (int)(255.999 * rgb_val);
+}
+
 int main() {
 
     // Constants
-    const int image_height = 144;
-    const int image_width = static_cast< int >( image_height * 16 / 9 );
+    // const int image_height = 144; // 402KB PPM
+    const int image_height = 360; // ??? PPM
+    // const int image_height = 480; // ??? PPM
+    // const int image_height = 720; // ??? PPM
+    // const int image_height = 1080; // 23MB
+    // const int image_height = 2160; // 89MB
+    const int image_width = (int)( image_height * 16 / 9 );
+    std::cout << "INFO: Image resolution is " << image_width << " by " << image_height << "\n";
+
     const std::string output_path = "images/out.ppm";
 
-    // Vars
+    // Initialise output string
     std::stringstream output_ppm;
-    // output_ppm << "P3\n" << image_width << " " << image_height << "\n255\n";
-
-    // EXAMPLE PPM
-    output_ppm << "P3\n"; // Format
-    output_ppm << "3 2\n"; // Resolution
-    output_ppm << "255\n"; // Max colour
-    output_ppm << "255  0   0       0   255 0       0   0   255\n"; // RGB values (row 1)
-    output_ppm << "255  255 0       255 255 255     0   0   0\n"; // RGB values (row 2)
+    output_ppm << "P3\n" << image_width << " " << image_height << "\n255\n";
 
 
-    // Save PPM    
-    std::cout << "INFO: Saving PPM data to " << output_path << "\n";
+    // Main logic
+    for (int j = image_height-1; j >= 0; --j) {
+        for (int i = 0; i < image_width; ++i) {
+
+            // TODO: Comment these operations
+            float r = double(i) / (image_width-1);
+            float g = double(j) / (image_height-1);
+            float b = 0.25;
+
+            int ir = map_float_to_rgb_int(r);
+            int ig = map_float_to_rgb_int(g);
+            int ib = map_float_to_rgb_int(b);
+
+            output_ppm << ir << "\t" << ig << "\t" << ib << "\t\t";
+        }
+        output_ppm << "\n";
+    }
+
+
+    // Save PPM
+    std::cout << "INFO: Saving PPM data to '" << output_path << "'\n";
     std::ofstream output_file_handler(output_path);
     output_file_handler << output_ppm.str();
     
-    
-
-
     return 0;
 }
