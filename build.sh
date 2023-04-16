1448a4376e0c4c0c356ad234fbc53012388ec099
JG <jasongalea98@outlook.com>
2023-04-16 13:35:33 +1000

    Simplify build script

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-### Prep
 # sudo apt update -y
 # sudo apt install build-essential netpbm
 
@@ -11,33 +10,19 @@ PPM_FILE="./images/out.ppm"
 PNG_FILE="./images/out.png"
 
 
-# ### Delete old binary
-# if [[ -f $BINARY_FILE ]]; then
-#     echo -e "\n==> $ rm $BINARY_FILE"
-#     rm $BINARY_FILE
-# fi
-
-
-# ### Delete old images
-# if [[ -f $PPM_FILE ]]; then
-#     echo -e "\n==> $ rm ./images/*.p*"
-#     rm ./images/*.p*
-# fi
-
-
 ### Compile
 echo -e "\n==> $ g++ main.cpp -o $BINARY_FILE"
 g++ main.cpp -o $BINARY_FILE
 
 
-### Run new binary
+### Run
 if [[ -f $BINARY_FILE ]]; then
     echo -e "\n==> $ $BINARY_FILE"
     $BINARY_FILE
 fi
 
 
-### Convert PPM file to PNG
+### Convert PPM --> PNG
 if [[ -f $PPM_FILE ]]; then
     echo -e "\n==> $ pnmtopng $PPM_FILE > $PNG_FILE"
     pnmtopng $PPM_FILE > $PNG_FILE
