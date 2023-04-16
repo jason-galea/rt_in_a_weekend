cf8043694109c2fe3f20abf8cf9a2ebeda975853
JG <jasongalea98@outlook.com>
2023-04-16 15:24:10 +1000

    Headers: Slowly discovering if I am insane

---
+++ src/vec3.cpp
@@ -0,0 +1,6 @@
+#include "vec3.h"
+
+// Constructer
+vec3::vec3(float x, float y, float z) {
+
+}
--- src/vec3.h
+++ src/vec3.h
@@ -1,23 +1,29 @@
-#ifndef VEC3_H
+#ifndef VEC3_H // If unique value is not defined, continue to include class definition
 #define VEC3_H
 
 #include <cmath>
 #include <iostream>
 
+
 class vec3 {
+    float xyz [3]; // Initialise empty array of floats with length 3
+
     public:
-        // What the fuck is "e" haha
-        // A completely arbitrary internal array?
-        // I guess 
+        // Constuctor(s)
+        // vec3() : xyz{0,0,0} {} // Default values, cool. Actually no, why does "xyz" exist instead of 3 parameters
+        // vec3(float x, float y, float z) : xyz{x, y, z} {} // What the fuck is with these braces
 
-        // Constuctor(s???)
-        vec3() : e{0,0,0} {} // Default values if empty args???
-        vec3(float e0, float e2, float e2) : e{e0, e1, e2} {} // Set public vars???
+        // Any reason to not do this?
+        // Apparently the convention is to put default args in the header
+        vec3(float x = 0, float y = 0, float z = 0) {}
 
-        // Getters/setters I guess
-        float x() const { return e[0]; }
-        float x() const { return e[1]; }
-        float x() const { return e[2]; }
+        // Getter functions
+        // float* xyz() const { return xyz; } // TODO: What type? Is this even useful
+        const float x() { return xyz[0]; }
+        const float y() { return xyz[1]; }
+        const float z() { return xyz[2]; }
+
+        // Utility functions
 };
 
-#endif
+#endif // Else, do not include class definition
